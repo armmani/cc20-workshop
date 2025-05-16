8e4b9bdbe5700aa69f05516a3cf2117511c43e61
armmani <arm.maniii@gmail.com>
2025-05-16 09:21:13 +0700

    Starter With DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl text-yellow-500">CODECAMPE20</h1>
+      <button className="btn btn-primary">OK</button>
+    </div>
+  )
+}
+  
+
+export default App
